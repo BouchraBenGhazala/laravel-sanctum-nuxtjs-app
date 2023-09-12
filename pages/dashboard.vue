2c3475a528b47fa7428a5e0b7f6d6d915fc7d53d
BouchraBenGhazala <ghazalabouchra@gmail.com>
2023-09-12 12:17:10 +0100

    added dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,21 +1,142 @@
 <template>
-  <section class="section">
-    <h2 class="title is-3 has-text-grey">
-      "Just start  <b-icon
-        icon="rocket"
-        size="is-large"
-      />"
-    </h2>
-    <h3 class="subtitle is-6 has-text-grey">
-      Author: <a href="https://github.com/anteriovieira">
-        Antério Vieira
-      </a>
-    </h3>
+    <section class="section">
+    <div class="container">
+      <h1 class="title">Dashboard</h1>
+      <!-- <div class="alert alert-warning" role="alert">Your tasks</div> -->
+    </div>
+    <div class="row row-cols-1 row-cols-md-2 g-4" >
+      <div class="col">
+  <div class="card h-100 bg-dark mb-3 mt-3">
+    <div class="card-header d-flex justify-content-center align-items-center">
+      <h4 class="fs-5 fw-normal fw-bold text-white">Number of tasks</h4>
+    </div>
+    <div class="card-body d-flex justify-content-center align-items-center">
+      <div class="bg-danger text-white rounded-circle p-4">
+        <h1 class="display-4">{{NumberOfTasks}}</h1>
+      </div>
+    </div>
+  </div>
+</div>
+
+  
+    <!-- 2nd -->
+    <div class="col">
+  <div class="card h-100 bg-dark mb-3 mt-3">
+    <div class="card-header d-flex justify-content-center align-items-center">
+      <h4 class="fs-5 fw-normal fw-bold text-white">Number of projects</h4>
+    </div>
+    <div class="card-body d-flex justify-content-center align-items-center">
+      <div class="bg-danger text-white rounded-circle p-4">
+        <h1 class="display-4">{{NumberOfProjects}}</h1>
+      </div>
+        </div>
+      </div>
+    </div>
+    <!-- 3rd -->
+    <div class="col">
+  <div class="card h-100 bg-dark mb-3 mt-3">
+    <div class="card-header d-flex justify-content-center align-items-center">
+      <h4 class="fs-5 fw-normal fw-bold text-white">Number of Technologies</h4>
+    </div>
+    <div class="card-body d-flex justify-content-center align-items-center">
+      <div class="bg-danger text-white rounded-circle p-4">
+        <h1 class="display-4">{{NumberOfTechnologies}}</h1>
+      </div>
+        </div>
+      </div>
+    </div>
+    <!-- 4rd -->
+    <div class="col">
+  <div class="card h-100 bg-dark mb-3 mt-3">
+    <div class="card-header d-flex justify-content-center align-items-center">
+      <h4 class="fs-5 fw-normal fw-bold text-white">Number of users</h4>
+    </div>
+    <div class="card-body d-flex justify-content-center align-items-center">
+      <div class="bg-danger text-white rounded-circle p-4">
+        <h1 class="display-4">{{NumberOfUsers}}</h1>
+      </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
   </section>
 </template>
 
+
 <script>
+import axios from 'axios';
 export default {
-  name: 'InspirePage'
+  data() {
+    return {
+      user: this.$auth.user.data,
+      Tasks:{},
+      Projects:{},
+      Technologies:{},
+      Users:{},
+    }
+  },
+  mounted(){
+            this.getTasks();
+            this.getProjects();
+            this.getTechnologies();
+            this.getUsers();
+        },
+        methods:{
+            getTasks(){
+                axios.get("http://localhost:8000/api/tasks").then(res=>{
+                    this.Tasks=res.data.message;
+                    console.log("data:");
+                    console.log(res)
+                });
+            },
+            getProjects(){
+                axios.get("http://localhost:8000/api/projets").then(res=>{
+
+                    this.Projects=res.data.message;
+                    console.log("data:");
+                    console.log(res)
+                    console.log("else...")
+                    console.log(Projects.title)
+                });
+            },
+            getTechnologies(){
+                axios.get("http://localhost:8000/api/technologies").then(res=>{
+
+                    this.isLoading =false,
+                    this.Technologies=res.data.message;
+                    console.log("data:");
+                    console.log(res)
+                });
+            },
+            getUsers(){
+                axios.get("http://localhost:8000/api/users").then(res=>{
+
+                    this.Users=res.data.message;
+                    console.log("data:");
+                    console.log(res)
+                });
+            },
+
+        },
+        computed: {
+          NumberOfTasks(){
+            return this.Tasks.length
+          },
+          NumberOfProjects(){
+            return this.Projects.length
+          },
+          NumberOfTechnologies(){
+            return this.Technologies.length
+          },
+          NumberOfUsers(){
+            return this.Users.length
+          },
+
+        },
 }
 </script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,121 @@
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title">Dashboard</h1>
+      <h1 class="title">Hi {{ user.name }}</h1>
+      <div class="alert alert-warning" role="alert">Your tasks</div></div>
+    <div class="row row-cols-1 row-cols-md-3 g-4" >
+    <div class="col" v-for="(task,index) in Tasks"  :key="index">
+        <div class="card h-100 bg-warning    mb-3 mt-3" v-if="user.id == task.user.id">
+            <div class="card-header d-flex justify-content-between">
+            <h4 class="fs-5 fw-normal fw-bold">{{ task.title}}</h4>
 
-      <p class="subtitle">Hi {{ user.name }}</p>
+
+        </div>
+        <div class="card-body">
+            <p class="card-text">{{ task.description}}</p>
+            <p>{{ task.priority}}</p>
+            <div class="d-inline-block bg-danger text-white px-1 rounded">
+            <small>{{ task.type}}</small>
+        </div>
+        <p class="card-text">{{ task.project.title}}</p>
+
+        </div>
+        <div class="card-footer">
+            <small class="text-muted "><span class="fw-bold">Creation:</span> {{ formatTimeAgo(task.created_at) }}</small>
+            <small class="text-muted"><span class="fw-bold">Last update:</span> {{ formatTimeAgo(task.updated_at) }}</small>
+            <small class="text-muted"><span class="fw-bold">Due date:</span> {{ formatTimeLeft(task.due_date) }}</small>
+        </div>
+        </div>
+
+    </div>
+  
+
     </div>
   </section>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   data() {
     return {
       user: this.$auth.user.data,
+      Tasks:{},
     }
   },
-  methods: {
-  },
+  mounted(){
+            this.getTasks();
+        },
+        methods:{
+            getTasks(){
+                axios.get("http://localhost:8000/api/tasks").then(res=>{
+
+  
+                    this.Tasks=res.data.message;
+                    console.log("data:");
+                    console.log(res)
+                });
+            },
+
+
+            formatTimeAgo(timestamp) {
+                const currentTime = new Date();
+                const updatedTime = new Date(timestamp);
+                const timeDifference = Math.floor((currentTime - updatedTime) / 60000); // Difference in minutes
+
+                if (timeDifference < 1) {
+                    return 'Just now';
+                } else if (timeDifference === 1) {
+                    return '1 minute ago';
+                } else if (timeDifference < 60) {
+                    return `${timeDifference} minutes ago`;
+                } 
+                else {
+        const hours = Math.floor(timeDifference / 60);
+        if (hours === 1) {
+          return '1 hour ago';
+        } else if (hours < 24) {
+          return `${hours} hours ago`;
+        } else {
+          const days = Math.floor(hours / 24);
+          if (days === 1) {
+            return '1 day ago';
+          } else {
+            return `${days} days ago`;
+          }
+
+            }
+        }
+    },
+    
+    formatTimeLeft(timestamp) {
+                const currentTime = new Date();
+                const dueTime = new Date(timestamp);
+                const timeDifference = Math.floor((dueTime - currentTime) / 60000); // Difference in minutes
+
+                const hours = Math.floor(timeDifference / 60);
+                const days = Math.floor(hours / 24);
+                if(days<1){
+                    return 'Overdue'
+                }
+                else if (days === 1) {
+                    return '1 day left';
+                } else {
+                    return `${days} days left`;
+                }
+
+            
+    
+    },
+        },
+        computed: {
+            routeParams() {
+            return this.$route.params;
+            },
+        },
 }
 </script>
+
+<style>
+
+</style>
